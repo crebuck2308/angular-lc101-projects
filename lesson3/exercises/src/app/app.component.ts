6ad8ddbd75a71f9b3d61ffff6c9f34b56ca6bbfb
Chris Rebuck <crebuck.apt@gmail.com>
2021-04-17 09:07:39 -0500

    Completed Week 10B Exercises

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,78 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled= true;
+
+
+  flightPath(width, height) {
+    if (width > 470000 || width < 0 ||height < 0 || height > 340000) {
+      this.color = "orange";
+    } else{
+      this.color = 'blue';
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + "px";
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+      
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + "px";
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + "px";
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    } else {
+        let movement = parseInt(rocketImage.style.bottom) + 10 + "px";
+        rocketImage.style.bottom = movement;
+        this.height = this.height + 10000;
+    }
+    this.flightPath(this.width, this.height);
+  };
+
+  handleTakeOff() {
+    let result = window.confirm("Are you sure the shuttle is ready for take off?");
+    if(result) {
+      this.color = "blue";
+      this.height = 10000;
+      this.width = 0;
+      this.message = "Shuttle in flight.";
+      this.takeOffEnabled = false;
+    }
+  };
+
+  handleLanding(rocketImage) {
+    let result = window.confirm("The shuttle is landing, landing gear engaged.")
+    if (result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = "The shuttle has landed."
+      rocketImage.style.left = '0px';
+      rocketImage.style.bottom = '0px';
+    }
+  };
+
+  missionAbort(rocketImage) {
+    let result = window.confirm("Are you sure you want to abort the mission?")
+    if(result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = "The mission has been aborted.  Shuttle returned to base."
+      rocketImage.style.left = '0px';
+      rocketImage.style.left = '0px';
+    }
+  };
+
+
+   
 }
+
+
+
